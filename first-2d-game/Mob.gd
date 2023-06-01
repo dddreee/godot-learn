6752342245d7fa3772128b8988a2c70b3de7ad34
dddreee <dddreee@163.com>
2023-06-01 22:04:52 +0800

    update

---
+++ first-2d-game/Mob.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+
+# 在 RigidBody2D 属性中，将 Gravity Scale 设置为 0，这样怪物就不会下坠。此外，在 CollisionObject2D 部分下，点击 Mask 属性并去除第一个复选框的勾选。这会确保怪物就不会彼此碰撞。
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var mob_types = $AnimatedSprite2D.sprite_frames.get_animation_names()
+	$AnimatedSprite2D.play(mob_types[randi() * mob_types.size()])
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	pass # Replace with function body.
--- first-2d-game/player.gd
+++ first-2d-game/player.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+signal hit
+
 @export var speed = 400
 var screen_size
 
@@ -7,8 +9,50 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity = Vector2.ZERO
+	if (Input.is_action_pressed("move_right")):
+		velocity.x += 1
+	if (Input.is_action_pressed("move_left")):
+		velocity.x -= 1
+	if (Input.is_action_pressed("move_up")):
+		velocity.y -= 1
+	if (Input.is_action_pressed("move_down")):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		
+	
+	
+	
+
+
+func _on_body_entered(body):
+	hide()
+	emit_signal("hit")
+	# 敌人每次击中 玩家时都会发出一个信号。我们需要禁用玩家的碰撞检测，确保我们不会多次触发 hit 信号。
+	$CollisionShape2D.set_deferred("disabled", true)
+	pass # Replace with function body.
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
